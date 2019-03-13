c9e8c177dc3d4f86cb4c8c87c54e7e625df4e1a7
aleksandar-stefanovic <theonewithideas@gmail.com>
2019-03-13 20:59:49 +0100

    Dodat osnovni grafički izgled aplikacije.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,46 @@
 import javafx.application.Application
-import tornadofx.App
-import tornadofx.View
-import tornadofx.borderpane
+import javafx.geometry.Pos
+import javafx.scene.layout.*
+import javafx.scene.paint.Color
+import tornadofx.*
 
 class MatfAsistentApp : App(MainView::class)
 
 class MainView: View("MatfAsistent") {
     override val root = borderpane {
+        prefWidth = 800.0
+        prefHeight = 600.0
 
+
+        center {
+            vbox {
+                style {
+                    border = Border(BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii(0.0), BorderWidths.DEFAULT))
+                }
+                alignment = Pos.CENTER
+                label("Sadržaj")
+            }
+        }
+
+        left {
+            vbox {
+                style {
+                    border = Border(BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii(0.0), BorderWidths.DEFAULT))
+                }
+                alignment = Pos.CENTER
+                label("Navigacija")
+            }
+        }
+
+        right {
+            vbox {
+                style {
+                    border = Border(BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii(0.0), BorderWidths.DEFAULT))
+                }
+                alignment = Pos.CENTER
+                label("Obaveštenja")
+            }
+        }
     }
 }
 
