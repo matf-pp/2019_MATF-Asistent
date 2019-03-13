9cfee34b225753295aecc7680091ebe2de0c7e40
aleksandar-stefanovic <theonewithideas@gmail.com>
2019-03-13 16:30:50 +0100

    Dodata TornadoFX biblioteka i osnove grafičkog interfejsa.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,16 @@
-fun main() {
-    println("Hello, MatfAsistent")
+import javafx.application.Application
+import tornadofx.App
+import tornadofx.View
+import tornadofx.borderpane
+
+class MatfAsistentApp : App(MainView::class)
+
+class MainView: View("MatfAsistent") {
+    override val root = borderpane {
+
+    }
+}
+
+fun main(args: Array<String>) {
+    Application.launch(MatfAsistentApp::class.java, *args)
 }
