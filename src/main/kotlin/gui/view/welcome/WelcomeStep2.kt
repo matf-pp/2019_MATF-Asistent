d6e31d0b314d74ac4c8f61f20760d300b0af71f6
aleksandar-stefanovic <theonewithideas@gmail.com>
2019-04-10 20:49:45 +0200

    Asinhroni pozivi prebačeni u repozitorijum.

--- src/main/kotlin/gui/view/welcome/WelcomeStep2.kt
+++ src/main/kotlin/gui/view/welcome/WelcomeStep2.kt
@@ -4,8 +4,12 @@ import data.CourseListItem
 import data.Repository
 import gui.tableview.NullSelectionModel
 import tornadofx.*
+import gui.view.welcome.WelcomeScreenWizard.ViewModel
+
 
 class WelcomeStep2 : View("Izbor kurseva") {
+
+    private val viewModel: ViewModel by inject()
 
     override val root = vbox {
         tableview(Repository.allAvailableCourses) {
@@ -20,5 +24,11 @@ class WelcomeStep2 : View("Izbor kurseva") {
             column("Naziv", CourseListItem::title)
             column("Broj ESPB", CourseListItem::espbPoints)
         }
+    }
+
+    // Ovo se izvršava kada se pritisne dugme "Sledeće"
+    override fun onSave() {
+        Repository.generateTimetables(viewModel)
+        super.onSave()
     }
 }
